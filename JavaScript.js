73fadaecc4b4e452c8aaaef855c0fdb0245108a8
Angel Lautaro Martinez <angelautaro2005@gmail.com>
2025-08-04 19:20:30 -0300

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -2,3 +2,8 @@ function toggleDetalle(id) {
   const div = document.getElementById(id);
   div.style.display = div.style.display === 'block' ? 'none' : 'block';
 }
+
+  function toggleMenu() {
+    const nav = document.getElementById("navLinks");
+    nav.classList.toggle("active");
+  }
